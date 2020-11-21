ff595620b05a21bfc997ce8874920c5165eaad1c
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-21 12:28:13 +0300

    setup index and app components

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "./Home"
+import Navbar from "./Navbar"
+
+function App() {
+	return (
+		<React.Fragment>
+			<Navbar />
+			<Home />
+		</React.Fragment>
+	)
+}
+export default App;
