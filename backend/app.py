3e9c086d2e9c6ebe9b4f3b38437b578034e387ee
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-21 11:23:57 +0300

    add db for the app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///twitter.db"
+
+db = SQLAlchemy(app)
+class Users(db.Model):
+	id = db.Column('student_id', db.Integer, primary_key=True)
+	username = db.Column(db.String(24))
+	email = db.Column(db.String(64))
+	pwd = db.Column(db.String(64))
+
+	def __init__(self, username, email, pwd):
+		self.username = username
+		self.email = email
+		self.pwd = pwd
+
 @app.route('/')
 def index():
+	db.create_all()
 	return "Hello, World!"
 
 if __name__ == "__main__":
