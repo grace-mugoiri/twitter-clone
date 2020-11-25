830572adf3cd7954b4c0136dd7778ef1271b1ec8
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-25 18:38:26 +0300

    add axios to post request to backend for login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
+	login = (e) => {
+		e.preventDefault();
+		axios
+			.post("http://localhost:5000/api/login", {
+				email: document.getElementById("email").value,
+				pwd: document.getElementById("password").value,
+			});
+			.then((res) => {
+				console.log(res.data)
+			});
+	};
+
   render() {
     return (
       <div className="w3-card-4" style={{ margin: "2rem" }}>
         <div className="w3-container w3-blue w3-center w3-xlarge"> LOGIN </div>
         <div className="w3-container">
-          <form action="">
+          <form onSubmit={this.Login}>
             <p>
               <label htmlFor="email"> Email </label>
               <input type="email" class="w3-input w3-border" id="email"/>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
 		<div className="w3-bar w3-black">
 			<a href="/" className="w3-bar-item w3-button"> Twitter</a>
 			<div style={{ float: "right"}}>
-				<a href="/" className="w3-bar-item w3-button"> login</a>
-				<a href="/" className="w3-bar-item w3-button"> Register</a>
+				<a href="/login" className="w3-bar-item w3-button"> login</a>
+				<a href="/register" className="w3-bar-item w3-button"> Register</a>
 			</div>
 		</div>
 	)
