9fa8a05a7a82c6128b45c7e1a31230171aebf79f
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-26 11:24:20 +0300

    add main page for logged in users

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="w3-container w3-jumbo" style={{ margin: "3rem" padding: "1rem" }}>
+					Tweets
+				</div>
+			</React.Fragment>
+		)
+	}
+}
+export default MainPage;
