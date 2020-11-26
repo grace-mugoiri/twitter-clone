89d8062fe01e00a4528026ddb858a10812c556d3
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-26 12:06:56 +0300

    add function component for displaying a tweet

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -4,7 +4,7 @@ class MainPage extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div className="w3-container w3-jumbo" style={{ margin: "3rem" padding: "1rem" }}>
+				<div className="w3-container w3-jumbo" style={{ margin: "3rem", padding: "1rem" }}>
 					Tweets
 				</div>
 			</React.Fragment>
