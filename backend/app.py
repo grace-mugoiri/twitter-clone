4c6164f0fd6d2cd0096a4e1688f3d247da33b22e
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-28 10:43:08 +0300

    add tweet model

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,9 @@ CORS(app)
 
 # DB
 db = SQLAlchemy(app)
-class Users(db.Model):
-    id = db.Column('student_id', db.Integer, primary_key = True) # primary_key makes it so that this value is unique and can be used to identify this record.
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True) # primary_key makes it so that this value is unique and can be used to identify this record.
     username = db.Column(db.String(24))
     email = db.Column(db.String(64))
     pwd = db.Column(db.String(64))
@@ -21,42 +22,95 @@ class Users(db.Model):
         self.email = email
         self.pwd = pwd
 
+def getUsers():
+    users = User.query.all()
+    return [{"id":i.id, "username":i.username, "email":i.email, "password":i.pwd} for i in users]
+
+def getUser(uid):
+    users = User.query.all()
+    user = list(filter(lambda x:x.id == uid, users))[0]
+    return {"id": user.id, "username": user.username, "email": user.email, "password": user.pwd}
+
+def addUser(username, email, pwd):
+    try:
+        user = User(username, pwd, email)
+        db.session.add(user)
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def removeUser(uid):
+    try:
+        user = User.query.get(uid)
+        db.session.delete(user)
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship('User', foreign_keys=uid)
+    title = db.Column(db.String(256))
+    content = db.Column(db.String(2050))
+
+def getTweets():
+    tweets = Tweet.query.all()
+    return [{"id": i.id, "title": i.title, "content": i.content, "user": getUser(i.uid)} for i in tweets]
+
+def getUserTweets(uid):
+    tweets = Tweet.query.all()
+    return [{"id":item.id, "userid":item.user_id, "title":item.title, "content":item.content}
+        for item in filter(lambda i: i.user_id == uid, tweets)]
+
+def addTweet(title, content, uid):
+    if (title and content and uid):
+        try:
+            user = list(filter(lambda i: i.id == uid, User.query.all()))[0]
+            twt = Tweet(title=title, content=content, user=user)
+            db.session.add(twt)
+            db.session.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    else:
+        return False
+
+def delTweet(tid):
+    try:
+        tweet = Tweet.query.get(tid)
+        db.session.delete(tweet)
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 @app.route("/")
 def react_index():
     return "Home Page"
 
+@app.route("/api/tweets")
+def get_tweets():
+	return jsonify(getTweets())
 
-def getUsers():
-    users = Users.query.all()
-    return [{"id":i.id, "username":i.username, "email":i.email, "password":i.pwd} for i in users]
-
-def addUser(username, email, pwd):
-    if (username and pwd and email):
-        try:
-            user = Users(username, pwd, email)
-            db.session.add(user)
-            db.session.commit()
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    else:
-        return False
-
-def removeUser(uid):
-    uid = request.json["id"]
-    if (uid):
-        try:
-            user = Users.query.get(uid)
-            db.session.delete(user)
-            db.session.commit()
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    else:
-        return False
+@app.route("/api/addtweet", methods=["POST"])
+def add_tweet():
+	try:
+		title = request.json["title"]
+		content = request.json["content"]
+		uid = request.json["uid"]
+		addTweet(title, content, uid)
+		return jsonify({"success": "true"})
+	except Exception as e:
+		print(e)
+		return jsonify({"error": "Invalid form"})
 
 @app.route("/api/login", methods=["POST"])
 def login():
