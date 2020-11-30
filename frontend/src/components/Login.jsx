37f1913feabbbcbca309fca76f9b3929556ae521
grace-mugoiri <gracemugoiri@gmail.com>
2020-11-30 11:32:09 +0300

    implement login function to Login jsx file

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,73 +1,20 @@
-// import React, { Component } from 'react';
-// import axios from 'axios';
-// import Alert from './Alert';
-
-// class Login extends Component {
-// 	state = { err: "" };
-
-// 	login = (e) => {
-// 		e.preventDefault();
-// 		axios.post("http://localhost:5000/api/login",
-// 			{
-// 				email: document.getElementById("email").value,
-// 				pwd: document.getElementById("password").value,
-// 			})
-// 			.then((res) => {
-// 				if (res.data.error) {
-// 					this.setState({ err: res.data.error })
-// 				}
-// 				else {
-// 					this.setState({ login: true })
-// 				}
-// 				});
-// 	};
-
-//   render() {
-//     return (
-//       <div className="w3-card-4" style={{ margin: "2rem" }}>
-//         <div className="w3-container w3-blue w3-center w3-xlarge"> LOGIN </div>
-// 				<div className="w3-container">
-// 					{this.state.err.length > 0 && (
-// 						<Alert message={`Check your form and try again! (${this.state.err})`} />
-// 					)}
-//           <form onSubmit={this.login}>
-//             <p>
-//               <label htmlFor="email"> Email </label>
-//               <input type="email" className="w3-input w3-border" id="email"/>
-//             </p>
-//             <p>
-//               <label htmlFor="password"> Password </label>
-//               <input type="password" className="w3-input w3-border" id="password"/>
-//             </p>
-//             <p>
-// 							<button type="submit" className="w3-button w3-blue">Login</button>
-// 							{this.state.register && <p>You are logged in!</p>}
-//             </p>
-//           </form>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default Login;
-
-
-
 import React, { Component } from "react";
 import axios from "axios";
+import Alert from './Alert';
+import { login } from '../login';
 
 class Login extends Component {
+	state = { err: "" };
     login = (e) => {
         e.preventDefault();
-        axios
-            .post("http://localhost:5000/api/login", {
-                email: document.getElementById("email").value,
-                pwd: document.getElementById("password").value,
-            })
-            .then((res) => {
-                console.log(res.data);
-            });
+		login(document.getElementById("email").value,
+			document.getElementById("password").value).then(r => {
+				if (r === true) {
+					this.setState({ login: true })
+				} else {
+					this.setState({ err: r })
+				}
+			})
     };
 
     render() {
@@ -76,7 +23,10 @@ class Login extends Component {
                 <div className="w3-container w3-blue w3-center w3-xlarge">
                     LOGIN
                 </div>
-                <div className="w3-container">
+				<div className="w3-container">
+					{this.state.err.length > 0 && (
+						<Alert message={`Check your form and try again! (${this.state.err})`} />
+					)}
                     <form onSubmit={this.login}>
                         <p>
                             <label htmlFor="email">Email</label>
@@ -96,8 +46,9 @@ class Login extends Component {
                         </p>
                         <p>
                             <button type="submit" class="w3-button w3-blue">
-                                Login
+								Login
                             </button>
+							{this.state.login && "You are logged in"}
                         </p>
                     </form>
                 </div>
