0cc6ed453fa4683dc8d884d5aa31489b315cadc8
grace-mugoiri <gracemugoiri@gmail.com>
2020-12-01 16:34:40 +0300

    get content from tinymce and post to db

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -7,7 +7,7 @@ class MainPage extends React.Component {
 	state = { tweets: [] }
 	componentDidMount() {
 		Axios.get("/api/tweets").then(res => {
-			this.setState({ tweets: res.data })
+			this.setState({ tweets: res.data.reverse() })
 		});
 	}
 	render() {
@@ -22,7 +22,7 @@ class MainPage extends React.Component {
 				</div>
 
 				<AddTweet />
-				
+
 				<div className="w3-container">
 					{this.state.tweets.length === 0 ? <p className="w3-xlarge w3-opacity" style={{ marginLeft: "2rem" }}>No tweets! Create one </p> : this.state.tweets.map((item, index) => {
 						return (
