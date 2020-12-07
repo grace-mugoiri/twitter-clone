ff2c33bc8386e656dfcc97ded769f3aa2ba2288d
grace-mugoiri <gracemugoiri@gmail.com>
2020-12-07 19:33:24 +0300

    add Switch on App ile

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,29 +3,33 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 import Login from "./Login";
 import Register from "./Register";
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './MainPage';
 import { check } from '../login';
 import Logout from './Logout';
 import UserSettings from './UserSettings';
 import NotFound from './NotFound';
+import './theme.css'
 
 
 function App() {
 	let [login, setLogin] = React.useState(false);
+
 	check().then(r => setLogin(r))
 
 	return (
 		<React.Fragment>
 			<Navbar />
 			<Router>
-				<Route path="/" exact> {login ? <MainPage/> : <Home/>} </Route>
-				{/* <Route path="/" exact component={MainPage} /> */}
-				<Route path="/login" exact component={Login} />
-				<Route path="/register" exact component={Register} />
-				<Route path="/logout" exact component={Logout} />
-				<Route path="/settings" exact component={UserSettings} />
-				<Route component={NotFound} />
+				<Switch>
+					<Route path="/" exact> {login ? <MainPage/> : <Home/>} </Route>
+					{/* <Route path="/" exact component={MainPage} /> */}
+					<Route path="/login" exact component={Login} />
+					<Route path="/register" exact component={Register} />
+					<Route path="/logout" exact component={Logout} />
+					<Route path="/settings" exact component={UserSettings} />
+					<Route component={NotFound} />
+				</Switch>
 			</Router>
 		</React.Fragment>
 	)
