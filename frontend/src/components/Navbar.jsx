d01b08a47e439f45745cc7800ddb2345bb399c59
grace-mugoiri <gracemugoiri@gmail.com>
2020-12-07 14:57:20 +0300

    modify navbar to have setting on a logged in user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
-	return (
-		<div className="w3-bar w3-black">
-			<a href="/" className="w3-bar-item w3-button"> Twitter</a>
-			<div style={{ float: "right"}}>
-				<a href="/login" className="w3-bar-item w3-button"> login</a>
-				<a href="/register" className="w3-bar-item w3-button"> Register</a>
-			</div>
-		</div>
-	)
+    let x = localStorage.getItem("token")
+    let a = {name: x ? "Settings" : "Login", link: x ? "/settings" : "/login"}
+    let b = {name: x ? "Logout" : "Register", link: x ? "/logout" : "/register"}
+
+    return (
+        <div className="w3-bar w3-black">
+            <a className="w3-bar-item w3-button" href="/">
+                Quickr
+            </a>
+            <div style={{ float: "right" }}>
+                <a className="w3-bar-item w3-button" href={a.link}>
+                    {a.name}
+                </a>
+                <a className="w3-bar-item w3-button" href={b.link}>
+                    {b.name}
+                </a>
+            </div>
+        </div>
+    );
 }
+
 export default Navbar;
