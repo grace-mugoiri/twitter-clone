0fa1a99cbc5f6c0e436f7f2338ce15d2ac422e9e
grace-mugoiri <gracemugoiri@gmail.com>
2021-02-09 17:25:31 +0300

    add feeds

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,103 +1,112 @@
 import React from 'react';
 import '../assets/styles/topbar.css';
-// import '../assets/images/GraceMugoiriPic.jpg
+import '../assets/images/GraceMugoiriPic.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faPhotoVideo, faPollH, faSmileBeam, faCalendarAlt, faHashtag, faPlus, faHome, faBell, faEllipsisH, faBookmark, faEnvelope, faListAlt, faUserCircle,
+  faPhotoVideo, faPollH, faSmileBeam, faCalendarAlt, faRetweet, faShare, faHeart, faComment, faVideo, faHashtag, faPlus, faHome, faBell, faEllipsisH, faBookmark, faEnvelope, faListAlt, faUserCircle
 } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, } from '@fortawesome/free-brands-svg-icons';
 function Home() {
   return (
     <>
       <div className="row">
-        <div className="column">
+        <div className="column-one">
           <div className="css-3">
+            <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+					<div className="css-3">
 						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-							<FontAwesomeIcon icon={faTwitter} />
+							<FontAwesomeIcon icon={faHome} />
 						</a>
-            <div className="css-3">
-							<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-								<FontAwesomeIcon icon={faHome} />
-							</a>
-              <div className="css-3">
-                <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-									<FontAwesomeIcon icon={faHashtag} />
-									</a>
-                <div className="css-3">
-                  <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-										<FontAwesomeIcon icon={faBell} />
-										</a>
-                  <div className="css-3">
-                    <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-											<FontAwesomeIcon icon={faEnvelope} />
-											</a>
-                    <div className="css-3">
-                      <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-												<FontAwesomeIcon icon={faBookmark} />
-												</a>
-                      <div className="css-3">
-                        <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-													<FontAwesomeIcon icon={faListAlt} />
-													</a>
-                        <div className="css-3">
-                          <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-														<FontAwesomeIcon icon={faUserCircle} />
-														</a>
-                          <div className="css-3">
-                            <a href="/api/tweets" target="_blank" rel="noopener noreferrer">
-															<FontAwesomeIcon icon={faEllipsisH} />
-															</a>
-														<div><button class="add-tweet-plus">
-															<FontAwesomeIcon icon={faPlus} />
-														</button></div>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faHashtag} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faBell} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faEnvelope} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faBookmark} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faListAlt} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faUserCircle} />
+						</a>
+					<div className="css-3">
+						<a href="/api/tweets" target="_blank" rel="noopener noreferrer">
+							<FontAwesomeIcon icon={faEllipsisH} />
+						</a>
+					<div>
+						<button className="add-tweet-plus">
+							<FontAwesomeIcon icon={faPlus} />
+						</button>
+					</div>
 
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
+					</div>
 
         </div>
-        <div className="column">
-          <div id="topbar">
-            Home
-            <div id="profile-tile" />
-            <div id="trending-tile" />
-            <div id="notice-tile" />
-            <div id="follow-tile" />
-            <div id="links-tile" />
+        <div className="column-two">
+          <div className="css-3">
+            <div id="topbar">
+              Home
+              <div id="profile-tile" />
+              <div id="trending-tile" />
+              <div id="notice-tile" />
+              <div id="follow-tile" />
+              <div id="links-tile" />
+
+            </div>
           </div>
           <hr />
 
           <div className="image">
-            {' '}
-            <img src="#" alt="" />
-            <div className="css-2">
-              {' '}
+            <a href="/profile"><img src="../assets/images/GraceMugoiriPic.jpg" alt="" /></a>
+            <div className="css-3">
               What's happening?
-              <div className="css-3">
-                media
-                <FontAwesomeIcon icon={faPhotoVideo} />
-                <div className="css-3">
-                  gif
+              <div className="css-3-others">
+                <a href="/api/tweets">
                   <FontAwesomeIcon icon={faPhotoVideo} />
-                  <div className="css-3">
-                    poll
-                    <FontAwesomeIcon icon={faPollH} />
-                    <div className="css-3">
-                      emoji
-                      <FontAwesomeIcon icon={faSmileBeam} />
-                      <div className="css-3">
-                        schedule
-                        <FontAwesomeIcon icon={faCalendarAlt} />
-                        <button>Tweet</button>
-                        <h1 className="w3-jumbo"> Twitter Clone </h1>
-                        <button className="w3-button w3-pink" style={{ marginRight: '1rem' }} onClick={() => (window.location = '/login')}>Login</button>
-                        <button className="w3-button w3-pink" onClick={() => (window.location = '/register')}> Register</button>
+                </a>
+                <div className="css-3-others">
+                  <a href="/api/tweets">
+                    <FontAwesomeIcon icon={faVideo} />
+                  </a>
+                  <div className="css-3-others">
+                    <a href="/api/tweets">
+                      <FontAwesomeIcon icon={faPollH} />
+                    </a>
+                    <div className="css-3-others">
+                      <a href="/api/tweets">
+                        <FontAwesomeIcon icon={faSmileBeam} />
+                      </a>
+
+                      <div className="css-3-others">
+                        <a href="/api/tweets">
+                          <FontAwesomeIcon icon={faCalendarAlt} />
+                        </a>
+                        <button id="tweet-button">Tweet</button>
+                        {/* <h1 className="w3-jumbo"> Twitter Clone </h1> */}
+                        {/* <button className="w3-button w3-pink" style={{ marginRight: '1rem' }} onClick={() => (window.location = '/login')}>Login</button> */}
+                        {/* <button className="w3-button w3-pink" onClick={() => (window.location = '/register')}> Register</button> */}
                       </div>
                     </div>
                   </div>
@@ -106,50 +115,136 @@ function Home() {
             </div>
           </div>
           <hr />
-          <div className="w3-container w3-blue" style={{ padding: '2rem', marginTop: '2rem' }}>
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
-          </div>
+					<div className="article">
+						<div><a href="/api" className="category">Computer Programming</a>
+							<a href="/api" className="content" >
+								<div className="photo"><img src="#" alt="" id="photo" />photo
+								<span>
+									<a href="/api">John Doe </a>
+									<a href="/api">@JohnDoe </a>
+									<span>16h</span>
+								</span>
 
-          <div className="w3-container w3-blue" style={{ padding: '2rem', marginTop: '2rem' }}>
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
-          </div>
+								<p>Lorem ipsum dolor sit amet</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
+									<span className="likes">
+										<a href="/api"><FontAwesomeIcon icon={faComment} />12</a>
+										<a href="/api"><FontAwesomeIcon icon={faRetweet} />27</a>
+										<a href="/api"><FontAwesomeIcon icon={faHeart} />212</a>
+										<a href="/api"><FontAwesomeIcon icon={faShare} /></a>
+								</span>
+								</div>
+								</a>
+						</div>
+					</div>
+					<hr/>
+					<div className="article">
+						<div><a href="/api" className="category">Computer Programming</a>
+							<a href="/api" className="content" >
+								<div className="photo"><img src="#" alt="" id="photo" />photo
+								<span>
+									<a href="/api">Mary Stew </a>
+									<a href="/api">@MaryStew </a>
+									<span>16h</span>
+								</span>
 
-          <div className="w3-container w3-blue" style={{ padding: '2rem', marginTop: '2rem' }}>
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
-          </div>
+								<p>Lorem ipsum dolor sit amet</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
+									<span className="likes">
+										<a href="/api"><FontAwesomeIcon icon={faComment} />12</a>
+										<a href="/api"><FontAwesomeIcon icon={faRetweet} />27</a>
+										<a href="/api"><FontAwesomeIcon icon={faHeart} />212</a>
+										<a href="/api"><FontAwesomeIcon icon={faShare} /></a>
+								</span>
+								</div>
+								</a>
+						</div>
+					</div>
+					<hr/>
+					<div className="article">
+						<div><a href="/api" className="category">Computer Programming</a>
+							<a href="/api" className="content" >
+								<div className="photo"><img src="#" alt="" id="photo" />photo
+								<span>
+									<a href="/api">Vin Tool </a>
+									<a href="/api">@VinTool </a>
+									<span>16h</span>
+								</span>
+
+								<p>Lorem ipsum dolor sit amet</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
+									<span className="likes">
+										<a href="/api"><FontAwesomeIcon icon={faComment} />12</a>
+										<a href="/api"><FontAwesomeIcon icon={faRetweet} />27</a>
+										<a href="/api"><FontAwesomeIcon icon={faHeart} />212</a>
+										<a href="/api"><FontAwesomeIcon icon={faShare} /></a>
+								</span>
+								</div>
+								</a>
+						</div>
+					</div>
+					<hr/>
+					<div className="article">
+						<div><a href="/api" className="category">Computer Programming</a>
+							<a href="/api" className="content" >
+								<div className="photo"><img src="#" alt="" id="photo" />photo
+								<span>
+									<a href="/api">Vin Tool </a>
+									<a href="/api">@VinTool </a>
+									<span>16h</span>
+								</span>
+
+								<p>Lorem ipsum dolor sit amet</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
+									<span className="likes">
+										<a href="/api"><FontAwesomeIcon icon={faComment} />12</a>
+										<a href="/api"><FontAwesomeIcon icon={faRetweet} />27</a>
+										<a href="/api"><FontAwesomeIcon icon={faHeart} />212</a>
+										<a href="/api"><FontAwesomeIcon icon={faShare} /></a>
+								</span>
+								</div>
+								</a>
+						</div>
+					</div>
+					<div className="article">
+						<div><a href="/api" className="category">Computer Programming</a>
+							<a href="/api" className="content" >
+								<div className="photo"><img src="#" alt="" id="photo" />photo
+								<span>
+									<a href="/api">Vin Tool </a>
+									<a href="/api">@VinTool </a>
+									<span>16h</span>
+								</span>
+
+								<p>Lorem ipsum dolor sit amet</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, qui nesciunt nihil expedita laudantium enim ipsa numquam illum nam? Accusantium quas aliquid laudantium maxime consequuntur fugiat iusto vitae deleniti officiis?</p>
+									<span className="likes">
+										<a href="/api"><FontAwesomeIcon icon={faComment} />12</a>
+										<a href="/api"><FontAwesomeIcon icon={faRetweet} />27</a>
+										<a href="/api"><FontAwesomeIcon icon={faHeart} />212</a>
+										<a href="/api"><FontAwesomeIcon icon={faShare} /></a>
+								</span>
+								</div>
+								</a>
+						</div>
+					</div>
         </div>
-        <div className="column">
-          column three
-          <div className="row2">
-            <div className="row-in">
-              <input type="text" placeholder="Search.." />
-            </div>
-            <div className="row-in">
-              Kenya Trends
-              <hr />
-              <span><ol>1. Trending</ol></span>
-              <span>#GrandVigil</span>
-              <span>6119 Tweets</span>
-              <hr />
-              <span><ol>1. Trending</ol></span>
-              <span>#GrandVigil</span>
-              <span>6119 Tweets</span>
-              <hr />
-              <span><ol>1. Trending</ol></span>
-              <span>#GrandVigil</span>
-              <span>6119 Tweets</span>
-              <hr />
-              <span><ol>1. Trending</ol></span>
-              <span>#GrandVigil</span>
-              <span>6119 Tweets</span>
-              <hr />
-              <span><ol>1. Trending</ol></span>
-              <span>#GrandVigil</span>
-              <span>6119 Tweets</span>
-              <hr />
+        <div className="column-three">
+						<div className="row-search">
+              <input type="text" placeholder="Search Twitter" />
+					</div>
+					<br/>
+            <div className="trends">
+              <a href="/api" id="trends">Kenya Trends</a>
+						<hr />
+						<div className="trending-news">
+              <div id="trending-news"><span> 1 </span></div>
+              <div className="dot"><span> . </span></div>
+              <div className="trending-name"><span> Trending </span></div>
+              <span id="hash">#Google Devs</span>
+              <div><span id="">3665 tweets</span></div>
+							<hr />
+							</div>
             </div>
             <div className="row-mid">
               Who to follow
@@ -158,8 +253,6 @@ function Home() {
               <span>@alipay</span>
               <button>Follow</button>
             </div>
-
-          </div>
 
         </div>
       </div>
